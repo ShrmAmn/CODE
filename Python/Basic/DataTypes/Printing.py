9b7d6ebfaed952eaddb8f33ccd201c7dc6a249f4
ShrmAmn <105295671+ShrmAmn@users.noreply.github.com>
2022-09-16 15:31:11 +0530

    Printing.py

---
+++ Python/Basic/DataTypes/Printing.py
@@ -0,0 +1,12 @@
+x = 'hello'
+print(x)
+# hello
+
+num = 12 
+name = 'sam'
+
+print('My number is : {one} , and my name is : {two} '.format(one = num , two = name))
+print('My number is : {} , and my name is : {} '.format(num , name))
+
+# My number is : 12, and my name is : sam 
+# My number is : 12, and my name is : sam 
