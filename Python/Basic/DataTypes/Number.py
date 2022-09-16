1cc2377191ca67e2fd5b59d29728d1444d095dc4
ShrmAmn <105295671+ShrmAmn@users.noreply.github.com>
2022-09-16 15:01:19 +0530

    Number.py

---
+++ Python/Basic/DataTypes/Number.py
@@ -0,0 +1,29 @@
+# Addition
+
+n = 4 + 5
+print(n)
+
+# Multiplication
+
+n = 1 * 3
+print(n)
+
+# Divide
+
+n = 1 / 2
+print(n)
+
+# Power
+
+n = 2 ** 4
+print(n)
+
+# MOD 
+
+n = 5 % 2
+print(n)
+
+# Distribution
+
+n = (2+3) * (5+5)
+print(n)
