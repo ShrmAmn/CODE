8ce383f29608a6936e2612331387b9e6e0b181a9
ShrmAmn <105295671+ShrmAmn@users.noreply.github.com>
2022-09-17 13:58:45 +0530

    Create If_Elif_Else_Statement.py

---
+++ Python/Basic/If_Elif_Else_Statement.py
@@ -0,0 +1,19 @@
+if 1 < 2 :
+    print('Yo!')
+# Yo!
+
+
+if 1 > 2:
+    print('First')
+else:
+    print('Last')
+# Last
+
+
+if 1 == 2:
+    print('First')
+elif 3 == 3:
+    print('Middle')
+else:
+    print('Last')
+# Middle
