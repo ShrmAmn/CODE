5e07b727a6ba53634b8c0ecfb3d7403525ec666b
ShrmAmn <105295671+ShrmAmn@users.noreply.github.com>
2022-09-17 14:03:00 +0530

    Create ForLoops.py

---
+++ Python/Basic/ForLoops.py
@@ -0,0 +1,28 @@
+arr = [1,2,3,4,5]
+
+for item in arr:
+    print(item)
+
+# 1 
+# 2
+# 3
+# 4
+# 5
+
+for item in arr:
+    print('Yep')
+    
+# Yep
+# Yep
+# Yep
+# Yep
+# Yep
+
+for jelly in arr:
+    print(jelly+jelly)
+    
+# 2
+# 4 
+# 6
+# 8
+# 10
